ca6eba1b4f4b64c8ae732b0cdb46f403021d55b9
x1x8j <1101219195@qq.com>
2025-10-28 10:51:43 +0800

    注释

--- kernel/start.c
+++ kernel/start.c
@@ -6,46 +6,31 @@
 void main();
 void timerinit();
 
-// entry.S needs one stack per CPU.
 __attribute__ ((aligned (16))) char stack0[4096];
 
-// entry.S jumps here in machine mode on stack0.
+
 void
 start()
 {
-  // set M Previous Privilege mode to Supervisor, for mret.
   unsigned long x = r_mstatus();
   x &= ~MSTATUS_MPP_MASK;
   x |= MSTATUS_MPP_S;
   w_mstatus(x);
 
-  // 设置栈指针
-  //__asm__ volatile("mv sp, %0" : : "r"(stack0 + 4096));
-
-  // 关键：设置 sscratch 指向内核栈顶
-  // w_sscratch((uint64)(stack0 + 4096));
-
-  // set M Exception Program Counter to main, for mret.
-  // requires gcc -mcmodel=medany
   w_mepc((uint64)main);
 
-  // disable paging for now.
   w_satp(0);
 
-  // delegate all interrupts and exceptions to supervisor mode.
   w_medeleg(0xffff);
   w_mideleg(0xffff);
   w_sie(r_sie() | SIE_SEIE | SIE_STIE);
 
-  // configure Physical Memory Protection to give supervisor mode
-  // access to all of physical memory.
   w_pmpaddr0(0x3fffffffffffffull);
   w_pmpcfg0(0xf);
 
-  // ask for clock interrupts.
+
   timerinit();
 
-  // keep each CPU's hartid in its tp register, for cpuid().
   int id = 0;
   w_tp(id);
 
@@ -53,24 +38,18 @@ start()
   uint64 stack_top = (uint64)(stack0 + (id + 1) * 4096);
   w_sscratch(stack_top);
 
-  
-  // switch to supervisor mode and jump to main().
   asm volatile("mret");
 }
 
-// ask each hart to generate timer interrupts.
+
 void
 timerinit()
 {
-  // enable supervisor-mode timer interrupts.
   w_mie(r_mie() | MIE_STIE);
   
-  // enable the sstc extension (i.e. stimecmp).
   w_menvcfg(r_menvcfg() | (1L << 63)); 
   
-  // allow supervisor to use stimecmp and time.
   w_mcounteren(r_mcounteren() | 2);
-  
-  // ask for the very first timer interrupt.
+
   w_stimecmp(r_time() + 1000000);
 }
