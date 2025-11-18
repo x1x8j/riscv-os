1bc788dd828f733c51bd646209c34dea2d75892f
x1x8j <1101219195@qq.com>
2025-11-18 12:32:57 +0800

    Update riscv.h

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -373,11 +373,7 @@ typedef uint64 *pagetable_t; // 512 PTEs
 #define PXSHIFT(level)  (PGSHIFT+(9*(level)))
 #define PX(level, va) ((((uint64) (va)) >> PXSHIFT(level)) & PXMASK)
 
-// one beyond the highest possible virtual address.
-// MAXVA is actually one bit less than the max allowed by
-// Sv39, to avoid having to sign-extend virtual addresses
-// that have the high bit set.
 #define MAXVA (1L << (9 + 9 + 9 + 12 - 1))
 
 
-#define PTE_COW (1L << 8)
+#define PTE_COW (1L << 8) // 表示该页是COW页
